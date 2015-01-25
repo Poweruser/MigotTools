e2b50221a7728394a14c11baab089a950012d5b5
md_5 <git@md-5.net>
2015-01-26 09:10:07 +1100

    Allow java 6 to be used [not advised]

--- src/main/java/org/spigotmc/builder/Builder.java
+++ src/main/java/org/spigotmc/builder/Builder.java
@@ -100,9 +100,8 @@ public class Builder
 
         if ( Float.parseFloat( System.getProperty( "java.class.version" ) ) < 51.0 )
         {
-            System.err.println( "*** ERROR *** BuildTools should be run with Java 7 or above." );
-            System.err.println( "*** ERROR *** Use java -version to check your Java version." );
-            return;
+            System.err.println( "*** WARNING *** You are not using Java 7. Although this will work, it is highly discouraged due to the security issues present." );
+            System.err.println( "*** WARNING *** Use java -version to check your version and update as soon as possible." );
         }
 
         try
